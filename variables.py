7945ba8ba9a3c5c504221a00aee0db070a6b3965
robert <robert5638@gmail.com>
2019-03-30 16:45:04 -0400

    python variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+
+name = "Robert"
+apellido = "Tagliavia"
+edad = 29
+
+print(name + " " + apellido)
+
+Name, lastName, Age = "Aaron", "Tagliavia", 2
+
+print(Name, lastName, Age)
+
+# constantes
+    #convencion
+    PI = 3,1416
+    MY_NAME = "Robert"
