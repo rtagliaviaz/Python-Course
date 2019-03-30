11cc3cbe8f7cfe0fc139e955349ae03280f4fedf
robert <robert5638@gmail.com>
2019-03-30 16:31:38 -0400

    python data types

---
+++ dataType.py
@@ -0,0 +1,41 @@
+
+#strings
+print('Hello')
+print("hello")
+
+#concatenar
+print("Hola" + " " +"como" +" " + "estas" + " " + "?")
+
+# numbers
+print(30)
+print(type(30))
+print(5+5)
+print(40-15)
+print(1.15)
+print(type(1.15))
+x=30+15
+print(x)
+
+#   lists -> mutable
+print(type(["mario", "luigi", "link", "kirby", "ganon"]))
+[1, 2 , 3 , 4]
+[]
+#   tuples -> inmutable
+(10, 20, 30, 40)
+()
+
+# dictionaries/objetos agrupar datos de una misma entidad
+{
+    "name":"ryan",
+    "apellido":"ray",
+    "apodo":"appu"
+}
+print(type({
+    "name":"ryan",
+    "apellido":"ray",
+    "apodo":"appu"
+}))
+{
+    "lat": 123213213,
+    "long": 12321321321
+}
