c9af44b0a04cad71beceaa616c28eb514d624a21
robert <robert5638@gmail.com>
2019-03-30 17:31:12 -0400

    python strings

---
+++ strings.py
@@ -0,0 +1,25 @@
+
+myStr = "Hello World"
+
+#dir que se puede hacer con cierto tipo de dato
+#print(dir(myStr))
+
+print(myStr.upper())
+print(myStr.lower())
+print(myStr.swapcase())
+print(myStr.replace("Hello", 'Hola'))
+print(myStr.count('l'))
+print(myStr.startswith('Hello'))
+print(myStr.split())
+print(myStr.find('l'))
+print(len(myStr))
+print(myStr.index('W'))
+print(myStr.isnumeric())
+print(myStr.isalpha())
+print(myStr[4])
+
+name = "Robert"
+
+print("My name is " +name)
+print(f"My name is {name}")
+print("My name is {0}".format(name))
