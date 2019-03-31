1ae2da4d601f670eeb163758f223ffb2a1b04064
robert <robert5638@gmail.com>
2019-03-31 15:38:41 -0400

    modules

---
+++ modules.py
@@ -0,0 +1,19 @@
+# own modules
+# third party modules
+# python modules
+
+# import datetime
+#
+# print(datetime.timedelta(minutes=120))
+
+# from datetime import timedelta, date
+# print(timedelta(minutes=100))
+# print(date.today())
+
+# import fmath
+# fmath.add(1,2)
+# fmath.substract(1,2)
+
+# from colorama import Fore, Style, init
+# init(convert=True)
+# print(Fore.GREEN+"Hello World")
